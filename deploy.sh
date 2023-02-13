6600fc58fdef4433827314fb29785dd24995eb35
Ysh <yangsheng_email@163.com>
2023-02-13 14:58:53 +0800

    node_modules变更

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ rm -rf ../my-blog-dist/*
 # 提交更新代码
 git add -A
 git commit -m 'node_modules变更'
-git push -u origin master:dev
+git push
 
 # 将build生成的dist目录拷贝至上一层目录中
 cp -rf docs/.vuepress/dist ../my-blog-dist/
