3f9e8e25a80e7052501898c18255a3d72715292f
Ysh <yangsheng_email@163.com>
2023-02-13 14:20:25 +0800

    node_modules变更

--- deploy.sh
+++ deploy.sh
@@ -24,4 +24,4 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Yangsheng-WEB/my-blog.git main:gh-pages
+git push -f git@github.com:Yangsheng-WEB/my-blog.git master:gh-pages
