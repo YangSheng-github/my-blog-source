58566e441831aa98eb9985e915ce3d7f29d16078
Ysh <yangsheng_email@163.com>
2023-02-16 00:12:39 +0800

    node_modules变更

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://USERNAME.github.io
-git remote set-url origin git@github.com:YangSheng-github/YangSheng-gitee.git
 git remote add origin git@github.com:YangSheng-github/YangSheng-gitee.git
 git push -f git@github.com:YangSheng-github/YangSheng-gitee.git gh-pages
 
